5434d399fa5d00095b174462a40e9b2b10e80529
Islam Khalitov <tu7m5rk73@aztu.local>
2022-05-14 13:09:29 +0400

    added

---
+++ WeekEndTask6/Program.cs
@@ -0,0 +1,86 @@
+﻿
+using System;
+
+namespace WeekEndTask6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Weapon weapon = new Weapon();
+
+
+            Console.Write("Capacity:");
+            weapon.BulletSize = Convert.ToInt32(Console.ReadLine());
+            Console.Write("BulletCount");
+            weapon.BulletCount = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Finish:");
+
+            bool check = true;
+            do
+            {
+                Console.WriteLine("0 - İnformasiya almaq üçün");
+                Console.WriteLine("1 - Shoot metodu üçün");
+                Console.WriteLine("2 - Fire metodu üçün");
+                Console.WriteLine("3 - GetRemainBulletCount metodu üçün");
+                Console.WriteLine("4 - Reload metodu üçün");
+                Console.WriteLine("5 - ChangeFireMode metodu üçün");
+                Console.WriteLine("6 - Proqramdan dayandırmaq üçün qısayoldur.");
+
+
+                int selector = Convert.ToInt32(Console.ReadLine());
+
+
+
+
+                switch (selector)
+                {
+                    case 0:
+                        {
+
+                        }
+                        break;
+                    case 1:
+                        {
+                            Console.WriteLine("Shoot metod:");
+                            weapon.Shoot();
+                        }
+                        break;
+                    case 2:
+                        {
+                            Console.WriteLine("Fire  metod:");
+                            weapon.Fire();
+                        }
+                        break;
+                    case 3:
+                        {
+                            Console.WriteLine("GetRemainBulletCount metod:");
+                            weapon.GetRemainBulletCount();
+                        }
+                        break;
+                    case 4:
+                        {
+                            Console.WriteLine("Reolad metod:");
+                            weapon.Reload();
+                        }
+                        break;
+
+                    case 5:
+                        {
+                            Console.WriteLine("ChangeFireMode");
+                            weapon.ChangeFireMode();
+                        }
+                        break;
+
+                    case 6:
+                        {
+                            check = false;
+                        }
+                        break;
+                }
+
+            } while (check);
+        }
+    }
+}
+
---
+++ WeekEndTask6/Weapon.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WeekEndTask6
+{
+    class Weapon
+    {
+        private int _count=0;
+
+        public int BulletSize { get; set; }
+        public int BulletCount
+        {
+            get
+            {
+                return _count;
+            }
+            set
+            {
+                if (value>0 && value<=30)
+                {
+                    _count = value;
+                }
+            }
+
+        }
+        public bool isSingle { get; set; }
+
+        //public Weapon(int bulletSize, int bulletCount)
+        //{
+        //    BulletSize = bulletSize;
+        //    BulletCount = bulletCount;
+        //}
+
+        public void Shoot()
+        {
+            Console.WriteLine(BulletCount-=1);
+        }
+        public void Fire()
+        {
+            if (isSingle==false)
+            {
+                Console.WriteLine(BulletCount = 0);
+            }
+        }
+
+        public void GetRemainBulletCount()
+        {
+            if (BulletCount<BulletSize)
+            {
+                Console.WriteLine(BulletSize-BulletCount);
+            }
+        }
+
+        public void Reload()
+        {
+            if (BulletCount < 30)
+            {
+                Console.WriteLine(BulletCount=30);
+            }
+        }
+
+        public void ChangeFireMode()
+        {
+            if (isSingle=true)
+            {
+                Console.WriteLine("Automatic");
+            }
+        }
+
+        public void ShowInfo()
+        {
+            Console.WriteLine($"BulleSize : {BulletSize}, BulletCount : {BulletCount}, isSingle : {isSingle}");
+        }
+    }
+}
